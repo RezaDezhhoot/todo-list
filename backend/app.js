b4ce14f79f13069f099c7437485be16537f560af
Reza Dezhhoot <rdezhhoot@gmail.com>
2025-11-16 17:46:30 +0330

    update

--- backend/app.js
+++ backend/app.js
@@ -3,25 +3,46 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 const tasks = [];
 const { v4 } = require("uuid")
+const multer = require("multer")
+const path = require("path")
 
 const app = express()
 const port = 3000;
 
+app.use(express.json({ limit: "100mb" }))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use(bodyParser.json())
 app.use(cors())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+})
+
 app.get('/list', (req, res) => {
     return res.status(200).json({
         tasks
     })
 })
-app.post('/add', (req, res) => {
+
+const upload = multer({ storage })
+app.post('/add', upload.single('file'), (req, res) => {
     const { title, description } = req.body
     const newTask = {
         title,
         description,
         done: false,
-        id: v4()
+        id: v4(),
     }
+    if (req.file) {
+        newTask['image'] = `http://localhost:3000/uploads/${req.file.filename}`
+    }
+    console.log(newTask);
+    
     tasks.push(newTask)
     res.status(201).json({
         task: newTask
@@ -42,7 +63,6 @@ app.patch('/done/:id', (req, res) => {
             message: 'not found'
         });
     }
-
 })
 
 app.delete('/delete/:id', (req, res) => {
